82185cfe49f9064f25cb2e2aafdf8100692890a4
jfoessettjr <133122595+jfoessettjr@users.noreply.github.com>
2023-06-11 15:17:06 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'C:\xampp\htdocs\CMPS\CMPS 261\Module 6\view\header.php'; ?>
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="product_manager">Product Manager</a>
+        </li>
+        <li>
+            <a href="product_catalog">Product Catalog</a>
+        </li>
+    </ul>
+</main>
+<?php include 'C:\xampp\htdocs\CMPS\CMPS 261\Module 6\view\footer.php'; ?>
