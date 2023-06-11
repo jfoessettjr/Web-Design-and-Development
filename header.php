ef88862aedaf22bd19a788bacf6713c68a79931c
jfoessettjr <133122595+jfoessettjr@users.noreply.github.com>
2023-06-11 15:24:14 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>My Beer Shop</title>
+    <link rel="stylesheet" type="text/css"
+          href="/book_apps/ch05_guitar_shop/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>My Beer Shop</h1></header>
