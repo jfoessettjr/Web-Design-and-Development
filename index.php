b25c1f61cf2bbf42171e81ce5f0dfd3c9822a159
jfoessettjr <133122595+jfoessettjr@users.noreply.github.com>
2023-06-11 15:22:59 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,55 @@
-<?php include 'C:\xampp\htdocs\CMPS\CMPS 261\Module 6\view\header.php'; ?>
-<main>
-    <h1>Menu</h1>
-    <ul>
-        <li>
-            <a href="product_manager">Product Manager</a>
-        </li>
-        <li>
-            <a href="product_catalog">Product Catalog</a>
-        </li>
-    </ul>
-</main>
-<?php include 'C:\xampp\htdocs\CMPS\CMPS 261\Module 6\view\footer.php'; ?>
+<?php
+require('C:\xampp\htdocs\CMPS\CMPS 261\Module 6\model\database.php');
+require('C:\xampp\htdocs\CMPS\CMPS 261\Module 6\model\product_db.php');
+require('C:\xampp\htdocs\CMPS\CMPS 261\Module 6\model\category_db.php');
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        $action = 'list_products';
+    }
+}
+
+if ($action == 'list_products') {
+    $category_id = filter_input(INPUT_GET, 'category_id', 
+            FILTER_VALIDATE_INT);
+    if ($category_id == NULL || $category_id == FALSE) {
+        $category_id = 1;
+    }
+    $category_name = get_category_name($category_id);
+    $categories = get_categories();
+    $products = get_products_by_category($category_id);
+    include('product_list.php');
+} else if ($action == 'delete_product') {
+    $product_id = filter_input(INPUT_POST, 'product_id', 
+            FILTER_VALIDATE_INT);
+    $category_id = filter_input(INPUT_POST, 'category_id', 
+            FILTER_VALIDATE_INT);
+    if ($category_id == NULL || $category_id == FALSE ||
+            $product_id == NULL || $product_id == FALSE) {
+        $error = "Missing or incorrect product id or category id.";
+        include('C:\xampp\htdocs\CMPS\CMPS 261\Module 6\errors\error.php');
+    } else { 
+        delete_product($product_id);
+        header("Location: .?category_id=$category_id");
+    }
+} else if ($action == 'show_add_form') {
+    $categories = get_categories();
+    include('product_add.php');    
+} else if ($action == 'add_product') {
+    $category_id = filter_input(INPUT_POST, 'category_id', 
+            FILTER_VALIDATE_INT);
+    $code = filter_input(INPUT_POST, 'code');
+    $name = filter_input(INPUT_POST, 'name');
+    $price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
+    if ($category_id == NULL || $category_id == FALSE || $code == NULL || 
+            $name == NULL || $price == NULL || $price == FALSE) {
+        $error = "Invalid product data. Check all fields and try again.";
+        include('C:\xampp\htdocs\CMPS\CMPS 261\Module 6\errors\error.php');
+    } else { 
+        add_product($category_id, $code, $name, $price);
+        header("Location: .?category_id=$category_id");
+    }
+}    
+?>
